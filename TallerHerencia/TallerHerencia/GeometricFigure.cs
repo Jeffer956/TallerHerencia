a5a8e1a96e974ece3ef6199ab1ab232b19f64f23
JEFFERSON ANDREY LAVERDE ACEVEDO <jeffersonlaverde1174263@correo.itm.edu.co>
2026-08-30 18:50:21 -0500

    Comming

---
+++ TallerHerencia/TallerHerencia/GeometricFigure.cs
@@ -0,0 +1,22 @@
+﻿namespace TallerHerencia;
+
+public abstract class GeometricFigure
+{
+    //Propiertes
+
+    public string Name { get; set; }
+
+
+    //Methods 
+
+    public abstract double GetArea();
+    public abstract double GetPerimeter();
+
+    public override string ToString()
+    {
+
+        return $"{Name}- Area:{GetArea:D4}/Perimeter:{GetPerimeter:D4}";
+    }
+
+}
+
