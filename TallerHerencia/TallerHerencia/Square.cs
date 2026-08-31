930ef50762d2a529cf0a63359983eb749f1c5c9d
JEFFERSON ANDREY LAVERDE ACEVEDO <jeffersonlaverde1174263@correo.itm.edu.co>
2026-08-30 20:17:54 -0500

    Comming1

---
+++ TallerHerencia/TallerHerencia/Square.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using TallerHerencia;
+
+namespace Backend;
+
+public class Square : GeometricFigure
+{
+    //Fields
+    private double _a;
+
+    //Contructor
+    public Square(double A)
+
+    {
+        _a = A;
+    }
+    //Propiety
+    public double A {
+        get => _a;
+        set => _a =ValidateA(A);
+    }
+
+    //Public methods
+
+    public override double GetArea()
+    {
+        return A * A;
+
+    }
+
+    public override double GetPerimeter()
+    {
+        return A * 4;
+    }
+
+    //Private methods
+    private double ValidateA(double A)
+    {
+        if (A <= 0)
+        {
+            throw new Exception($"{A},Enter a valid number .");
+        }
+        return A;
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+//Private methods
+
+
+
+
+
+
+
