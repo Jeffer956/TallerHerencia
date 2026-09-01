28ca23fb2f401a0fc23df6075c2c0185ce34eae2
JEFFERSON ANDREY LAVERDE ACEVEDO <jeffersonlaverde1174263@correo.itm.edu.co>
2026-09-01 08:44:41 -0500

    Conflicto limpio

--- TallerHerencia/TallerHerencia/GeometricFigure.cs
+++ TallerHerencia/TallerHerencia/GeometricFigure.cs
@@ -15,9 +15,8 @@ public abstract class GeometricFigure
     public override string ToString()
 
     {
-
-        return $"{Name,-15}=> Area.....:   {GetArea(),11:N5}       Perimeter:   {GetPerimeter(),10:N5}";
-    }
+       return $"{Name,-15}=> Area.....:   {GetArea(),11:N5}       Perimeter:   {GetPerimeter(),10:N5}";
+            }
 
 }
 
