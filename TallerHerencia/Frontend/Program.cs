5715847dc8860a330d46ab3746ca49d75f79c83b
JEFFERSON ANDREY LAVERDE ACEVEDO <jeffersonlaverde1174263@correo.itm.edu.co>
2026-09-01 08:41:56 -0500

    Finish

--- TallerHerencia/Frontend/Program.cs
+++ TallerHerencia/Frontend/Program.cs
@@ -1,21 +1,31 @@
 ﻿using Backend;
 using TallerHerencia;
 
-var circle = new Circle(name: nameof(Circle), r: 5);
-var square = new Square(name: nameof(Square), a: 10);
-var rhombus = new Rhombus(name: nameof(Rhombus), a: 5, d1: 7, d2: 10);
-var kite = new Kite(name: nameof(Kite), a: 7, b: 8, d1: 6, d2: 5);
-var rectangle = new Rectangle(name: nameof(Rectangle), a: 4.568, b: 67.790);
-var parallelogram = new Parallelogram(name: nameof(Parallelogram), a: 14.65, b: 54.67, h: 23.09);
-var triangle = new Triangle(name: nameof(Triangle), a: 45.56, b: 12.34, c: 27.09, h: 15);
-var trapeze = new Trapeze(name: nameof(Trapeze), a: 10, b: 20, c: 30, d: 40, h: 20);
+// Crear figuras geométricas
+var circulo = new Circle(name: "Círculo", r: 5);
+var cuadrado = new Square(name: "Cuadrado", a: 10);
+var rombo = new Rhombus(name: "Rombo", a: 5, d1: 7, d2: 10);
+var cometa = new Kite(name: "Cometa", a: 7, b: 8, d1: 6, d2: 5);
+var rectangulo = new Rectangle(name: "Rectángulo", a: 4.568, b: 67.790);
+var paralelogramo = new Parallelogram(name: "Paralelogramo", a: 14.65, b: 54.67, h: 23.09);
+var triangulo = new Triangle(name: "Triángulo", a: 45.56, b: 12.34, c: 27.09, h: 15);
+var trapecio = new Trapeze(name: "Trapecio", a: 10, b: 20, c: 30, d: 40, h: 20);
 
-var figures = new List<GeometricFigure>
+// Lista de figuras
+var figuras = new List<GeometricFigure>
 {
-    circle, square, rhombus, kite, rectangle, parallelogram, triangle, trapeze
+    circulo,
+    cuadrado,
+    rombo,
+    cometa,
+    rectangulo,
+    paralelogramo,
+    triangulo,
+    trapecio
 };
 
-foreach (var figure in figures)
+// Mostrar cada figura en consola
+foreach (var figura in figuras)
 {
-    Console.WriteLine(figure);
+    Console.WriteLine(figura);
 }
